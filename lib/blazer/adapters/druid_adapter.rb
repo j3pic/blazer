6629a98fd95f53c45ea86331ece661450db4be4f
Jeremy Phelps <jeremy.phelps@instacart.com>
2017-04-06 15:55:57 -0700

    Wrote a universal structure-flattener to flatten all Druid results into tables. This doesn't work well with Druid's segmentMetadata query type, which has keys whose names represent column names, and whose values are hashes with several keys worth of information.
    Also implemented a "select" extension to Druid's syntax. This
    is a post-processed directive whose syntax is as follows:
    
    {
       "select": ["column1","column2"]
    }
    
    It removes all columns not in the list, and ensures that the
    columns that do appear show up in the order specified.

--- lib/blazer/adapters/druid_adapter.rb
+++ lib/blazer/adapters/druid_adapter.rb
@@ -60,7 +60,7 @@ module Blazer
       
       def initialize(data_source)
         @url=data_source.settings["url"]
-        @extended_operators = [:select]
+        @extended_operators = [:select,:raw,:orderBy,:orderByDirection]
       end
       
       def with_symbol_keys(hash)
@@ -75,15 +75,7 @@ module Blazer
         result = HTTParty.post "#{@url}/druid/v2/?pretty", body: no_extended_operators(hash).to_json, headers: {"Content-Type" => "application/json"}
         result
       end
-      
-      def render_timeseries(orig_query,data)
-        # [columns, values, error=nil]
-        [["timestamp"] + data[0]["result"].keys,
-         data.map do |row|
-           [row["timestamp"]] + row["result"].values
-         end, nil]
-      end
-      
+            
       def flatten_gently(array)
         result = []
         array.each do |elem|
@@ -95,16 +87,7 @@ module Blazer
         end
         result
       end
-      
-      def render_topN(orig_query,data)
-        [["timestamp"] + data[0]["result"][0].keys,
-         flatten_gently(data.map do |timestamp_row|
-                          timestamp_row["result"].map do |inner_row|
-                            [timestamp_row["timestamp"]] + inner_row.values
-                          end
-                        end),nil]
-      end
-    
+          
       def extended_operators(hash)
         hash.slice(*@extended_operators)
       end
@@ -113,6 +96,96 @@ module Blazer
         hash.except(*@extended_operators)
       end
 
+      def select_invalid_columns?(selected_columns, columns)
+        selected_columns.each { |col|
+          if ([col] & columns).length == 0
+            return col
+          end
+        }
+        false
+      end
+      
+      def select(selected_columns, columns, rows, error)
+        invalid = select_invalid_columns?(selected_columns,columns)
+        if invalid
+          [[],[],"Invalid column selected: #{invalid}"]
+        else
+          [selected_columns, rows.map do |row|
+             result=[]
+             selected_columns.map do |colname|
+               ix = columns.index colname
+               result.push(row[ix])
+             end
+             result
+           end,nil]
+        end
+      end
+
+      def order_by(sort_by_columns, ascendingp, columns, rows, error)
+        invalid = select_invalid_columns?(sort_by_columns, columns)
+        indices = sort_by_columns.map do |col|
+          columns.index(col)
+        end
+        cmp = if ascendingp
+                lambda { |a,b| a<b }
+              else
+                lambda { |a,b| a>b }
+              end
+        if invalid
+          [[],[],"Invalid orderBy column: #{invalid}"]
+        else
+          puts "Sorting #{rows} by #{sort_by_columns}"
+          sorted_rows = rows.sort_by do |row_a,row_b|
+            puts "sort_by block called with #{row_a} and #{row_b}"
+            catch :verdict do
+              indices.each do |ix|
+                puts "Comparing #{row_a[ix]} with #{row_b[ix]}"
+                if cmp.call(row_a[ix],row_b[ix])
+                  throw :verdict, true
+                elsif cmp.call(row_b[ix],row_a[ix])
+                  throw :verdict, false
+                end
+              end
+              throw :verdict, false
+            end
+          end
+          [columns,sorted_rows,error]
+        end
+      end
+
+      def postprocess_result(raw_result,parsed_statement)
+        if raw_result.is_a?(Hash)
+          [[], [], raw_result["error"] + ": " + raw_result["errorMessage"] + " (error of type " + raw_result["errorClass"] + ")"]
+        elsif raw_result.length == 0
+          [[],[],nil]
+        elsif parsed_statement[:raw]
+          [["raw_result"],[[raw_result.to_json]],nil]
+        else
+          flattened = flatten_structure(raw_result).uniq
+          columns=flattened[0].keys
+          result=[columns, flattened.map { |row|
+                    row.values
+                  }, nil]
+          if parsed_statement[:select]
+            result=select(parsed_statement[:select], *result)
+          end
+          if parsed_statement[:orderBy]
+            ascendingp = case parsed_statement[:orderByDirection]
+                         when "ascending"
+                           true
+                         when nil
+                           true
+                         when "descending"
+                           true
+                         else
+                           return [[],[],"Invalid orderByDirection: #{parsed_statement[:orderByDirection]}. Valid values are \"ascending\" or \"descending\""]
+                         end
+            result=order_by(parsed_statement[:orderBy],ascendingp,*result)
+          end
+          result
+        end
+      end
+      
       def run_statement(statement, comment)
         parsed_statement = with_symbol_keys(JSON.parse statement)
         if parsed_statement[:intervals]
@@ -122,22 +195,155 @@ module Blazer
             end.join("/")
           end
         end
-        raw_result = query_druid(parsed_statement)
-        if raw_result.is_a?(Hash)
-          [[], [], raw_result["error"] + ": " + raw_result["errorMessage"] + " (error of type " + raw_result["errorClass"] + ")"]
-        elsif raw_result.length == 0
-          [[],[],nil]
-        elsif parsed_statement[:queryType] == "timeseries" 
-          render_timeseries(parsed_statement, raw_result)
-        elsif parsed_statement[:queryType] == "topN"
-          render_topN(parsed_statement,raw_result)
-        else
-          columns=raw_result[0].keys
-          [columns, raw_result.map { |row|
-             row.values
-           }, nil]
+        postprocess_result(query_druid(parsed_statement),parsed_statement)
+      end
+
+      def join_hashes(first_hash,other_hash, supersede: false)
+        result=first_hash.except
+        other_hash.each { |k,v|
+          if (!result[k]) || supersede
+            result[k] = v
+          end
+        }
+        result
+      end
+
+      def join_hash_to_array_of_hashes(hash, array)
+        array.map do |hash_n|
+          join_hashes hash, hash_n
         end
       end
+
+      def join_hash_to_flat_array(hash, k, array)
+        array.map do |n|
+          next_hash = hash.except
+          next_hash[k] = n
+          next_hash
+        end
+      end
+
+      def join_hash_array_to_hash_array(hasharr_a, hasharr_b)
+        flatten_gently(hasharr_a.map do |hash|
+                         join_hash_to_array_of_hashes(hash,hasharr_b)
+                       end)
+      end
+
+      def join_hash_array_to_flat_array(hasharr, flat_key, flatarr)
+        flatten_gently(hasharr.map do |hash|
+                         join_hash_to_flat_array hash, flat_key, flatarr
+                       end)
+      end
+
+      def join_to_flat_array(hash_or_hasharr, key, flatarr)
+        if hash_or_hasharr.is_a?(Hash)
+          join_hash_to_flat_array(hash_or_hasharr, key, flatarr)
+        else
+          join_hash_array_to_flat_array(hash_or_hasharr, key, flatarr)
+        end
+      end
+
+      def join_to_hash_array(hash_or_hasharr, hash_arr)
+        if hash_or_hasharr.is_a?(Hash)
+          join_hash_to_array_of_hashes(hash_or_hasharr, hash_arr)
+        else
+          join_hash_array_to_hash_array(hash_or_hasharr, hash_arr)
+        end
+      end
+
+      def join_to_hash(hash_or_hasharr, hash)
+        if hash_or_hasharr.is_a?(Hash)
+          join_hashes hash_or_hasharr, hash
+        else
+          join_to_hash_array(hash_or_hasharr, hash)
+        end
+      end
+
+      def flat?(obj)
+        if obj.is_a?(Hash)
+          obj.each do |k,v|
+            if v.is_a?(Hash) || v.is_a?(Array)
+              return false
+            end
+          end
+          return true
+        elsif obj.is_a?(Array)
+          obj.each do |elem|
+            if !elem.is_a?(Hash)
+              return false
+            end
+          end
+          return true
+        else
+          true
+        end
+      end
+      
+      def flatten_hash_structure_1(hash)
+        result={}
+        hashes=[]
+        flat_arrays={}
+        hash_arrays=[]
+        hash.each do |k,v|
+          v = flatten_structure(v)
+          if v.is_a?(Hash)
+            hashes.push(v)
+          elsif v.is_a?(Array)
+            v = flatten_structure(v.flatten)
+            if v.length == 0 || !(v[0].is_a?(Hash))
+              flat_arrays[k]=v
+            else
+              hash_arrays.push(v.map do |elem|
+                                 flatten_structure(elem)
+                               end)
+            end
+          else
+            result[k] = v
+          end
+        end
+        
+        flat_arrays.each do |ek,ev|
+          result = join_to_flat_array(result, ek, ev)
+        end
+        hash_arrays.each do |elem|
+          result = join_to_hash_array(result,elem)
+        end
+        hashes.each do |elem|
+          result = join_to_hash(result, elem)
+        end
+        result
+      end
+
+      def flatten_hash_structure(hash)
+        result=hash
+        while(!flat?(result))
+          result = flatten_hash_structure_1(result)
+        end
+        result
+      end
+
+      def flatten_hash_array(hasharr)
+        result=[]
+        hasharr.each do |hash|
+          new_hash = flatten_hash_structure(hash)
+          if new_hash.is_a?(Array)
+            result += new_hash
+          else
+            result.push(new_hash)
+          end
+        end
+        result
+      end
+
+      def flatten_structure(s)
+        if s.is_a?(Hash)
+          flatten_hash_structure(s)
+        elsif s.is_a?(Array)
+          flatten_hash_array(s)
+        else
+          s
+        end
+      end
+
       
       def adapter_name
         "DruidAdapter"
